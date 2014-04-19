65c3c34aff3f49d287da4a282e306412fead7c2a
Boriphuth Saensukphattraka <boriphuth.saensukphattraka@live.com>
2014-04-19 22:42:19 +0700

    First commit

---
+++ FileUploadTest1/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FileUploadTest1.Models;
+
+namespace FileUploadTest1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Picture picture)
+        {
+            foreach (var file in picture.Files)
+            {
+                if (file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    var path = Path.Combine(Server.MapPath("~/Content/images"), fileName);
+                    file.SaveAs(path);
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
